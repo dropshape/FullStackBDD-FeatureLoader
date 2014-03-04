a0a1fd3dc847286e48f9c11c0bd4a827f2e41809
DropShape <admin@dropshape.com>
2014-03-04 01:03:32 +0000

    Adding grunt test capabilities

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,20 @@ module.exports = function (grunt) {
 
     //Default task runs tests, jshint and watches for changes.
     grunt.registerTask('default',
-        ['jasmine_node', 'jshint', 'watch']);
+        ['test', 'jshint', 'watch']);
 
     //Just run tests
-    grunt.registerTask('test', 'jasmine_node');
-
-    //Alias for default
-    grunt.registerTask('test:watch', 'default');
+    grunt.registerTask('test', 'shell:test');
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         //Tests
-        jasmine_node: {
-            options: {
-                forceExit: true,
-                verbose: true
-            },
-            files: { src: 'test/**/*.spec.js'}
+        shell: {
+            test:{
+                command: './node_modules/cucumber/bin/cucumber.js test/ -r test/step_definitions/ --format pretty --tags ~@ignore | ./node_modules/bunyan/bin/bunyan -o short'
+            }
         },
 
         //Clean code.
@@ -29,21 +24,21 @@ module.exports = function (grunt) {
             options: {
                 jshintrc: '.jshintrc'
             },
-            files: { src: ['index.js','lib/**/*.js', 'test/**/*.js']}
+            files: { src: ['index.js', 'lib/**/*.js', 'test/**/*.js']}
         },
 
         //Files to watch and actions to take when they are changed.
         watch: {
-            files: ['index.js','lib/**/*.js', 'test/**/*.spec.js'],
-            tasks: ['jshint', 'jasmine_node']
+            files: ['index.js', 'lib/**/*.js', 'test/**/*.spec.js'],
+            tasks: ['test', 'jshint']
         }
     });
 
     // Load the plugins
     // Watch the file system for changes.
     grunt.loadNpmTasks('grunt-contrib-watch');
-    // Runs tests.
-    grunt.loadNpmTasks('grunt-jasmine-node');
+    //tests
+    grunt.loadNpmTasks('grunt-shell');
     // Clean code validator.
     grunt.loadNpmTasks('grunt-contrib-jshint');
 };
